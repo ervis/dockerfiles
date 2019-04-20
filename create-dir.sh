3efa8859c19c007112cb14055b3192c81ff570e1
Ervis Zyka <erviszyka@gmail.com>
2019-04-20 13:37:28 +0300

    Remove unused containers

--- create-dir.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p /srv/docker/{gitlab, jenkins, postgresql}
-
-
-echo "127.0.1.1 gitlab.local" >> /etc/hosts
-echo "127.0.1.1 jenkins.local" >> /etc/hosts
-echo "127.0.1.1 mattermost.local" >> /etc/hosts
